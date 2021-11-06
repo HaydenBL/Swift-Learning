7526626da1fca24771d42e7ffb0a14e2e8a4af1c
Hayden Blaise <haydenlueck@gmail.com>
2021-11-05 19:32:37 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hayden Lueck on 2021-11-05.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
